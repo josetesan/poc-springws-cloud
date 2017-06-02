c507a3271752c9a8528b13fddbe2f0e654313ceb
Jose Luis Sanchez <muallin@gmail.com>
2017-06-02 19:33:28 +0200

    Initial skeleton

---
+++ soapserver/src/main/java/com/ingdirect/pocspringws/soapserver/SoapserverApplication.java
@@ -0,0 +1,12 @@
+package com.ingdirect.pocspringws.soapserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapserverApplication.class, args);
+	}
+}
