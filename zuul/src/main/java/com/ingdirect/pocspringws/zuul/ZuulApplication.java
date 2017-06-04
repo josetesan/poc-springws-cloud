a99bde6aee3d9b45bdb9dfc771edad0d1fcc9bc7
Jose Luis Sanchez <muallin@gmail.com>
2017-06-04 22:07:16 +0200

    Enabled zuul

--- zuul/src/main/java/com/ingdirect/pocspringws/zuul/ZuulApplication.java
+++ zuul/src/main/java/com/ingdirect/pocspringws/zuul/ZuulApplication.java
@@ -2,8 +2,11 @@ package com.ingdirect.pocspringws.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class ZuulApplication {
 
 	public static void main(String[] args) {
